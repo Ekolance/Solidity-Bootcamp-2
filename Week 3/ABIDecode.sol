6758b71c0fa9e7df9f50df4c4b7584dfa10ac483
poamen <poamen@appzonegroup.com>
2022-10-30 21:16:09 +0100

    added week 3

---
+++ Week 3/ABIDecode.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/*
+
+abi.encode encodes data into bytes.
+
+abi.decode decodes bytes back into data.
+*/
+
+
+contract AbiDecode {
+    struct MyStruct {
+        string name;
+        uint[2] nums;
+    }
+
+    function encode(
+        uint x,
+        address addr,
+        uint[] calldata arr,
+        MyStruct calldata myStruct
+    ) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+
+    function decode(bytes calldata data)
+        external
+        pure
+        returns (
+            uint x,
+            address addr,
+            uint[] memory arr,
+            MyStruct memory myStruct
+        )
+    {
+        // (uint x, address addr, uint[] memory arr, MyStruct myStruct) = ...
+        (x, addr, arr, myStruct) = abi.decode(data, (uint, address, uint[], MyStruct));
+    }
+}
