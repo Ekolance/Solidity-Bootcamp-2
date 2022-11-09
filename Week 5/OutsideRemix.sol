f6f55cd025eefb73cb770ae06cde78486ccf01d8
poamen <poamen@appzonegroup.com>
2022-11-09 18:51:59 +0100

    added week 4

---
+++ Week 5/OutsideRemix.sol
@@ -0,0 +1,18 @@
+
+/**
+CIntroduction/ setup for writing smart contracts on Vscode. 
+hardhat, truffle
+ethers.js, web3.js
+hardhat localnode
+remix
+ganache
+ethereum testnets
+faucets
+metamask
+alchemy
+infura,
+ABI
+GraphQL
+checking on remix
+Writing smart contract tests
+**/
